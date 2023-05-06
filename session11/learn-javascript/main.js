6335694a1bd5797fb43c6581922fa0cc01ef4a5e
longnm2000 <longnguyenmanh20052000@gmail.com>
2023-05-06 09:33:45 +0700

    session 11

---
+++ session11/learn-javascript/main.js
@@ -0,0 +1,10 @@
+function personalInfo() {
+    let fullName = prompt("Họ tên");
+    let age = prompt("Tuổi");
+    let location = prompt("Địa chỉ");
+    alert(`Họ tên :${fullName}, Tuổi: ${age}, Địa chỉ: ${location}`);
+    console.log()
+}
+
+personalInfo();
+
