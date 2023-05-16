99c5df4aeee8388851afc7982392177c39997d24
longnm2000 <longnguyenmanh20052000@gmail.com>
2023-05-16 08:41:04 +0700

    code

---
+++ JAVASCRIPT_sessions/session20/main.js
@@ -0,0 +1,15 @@
+let todoList = ["Go to work at 8 am", "Go to sleep at 12 pm"];
+
+function printTodo() {
+  for (let i = 0; i < todoList.length; i++) {
+    console.log(`${i + 1}. ${todoList[i]}`);
+  }
+}
+
+printTodo();
+
+function printTodo2(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${i + 1}. ${arr[i]}`);
+  }
+}
